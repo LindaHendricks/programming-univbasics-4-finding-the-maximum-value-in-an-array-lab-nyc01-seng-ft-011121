8740d1efe5230bd1baf15ecc413d7b1aa618d743
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-18 00:25:40 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   count = 0 
   
   while count < array.length do 
-    if array[count] == array[count]
+    if array[count] < array[count]
       return count
     end
     count += 1 
